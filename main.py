5b940762a73d2eee37d0eed9e3631572d22f3551
neuroCar <n2194050189@gmail.com>
2026-02-18 13:22:21 -0600

    Fix potential mistakes with subprocess and move to PySide6

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import os, platform, subprocess
-import customtkinter as ctk
-from CTkFileDialog import askopenfilename
+from PySide6.QtWidgets import *
+from PySide6.QtGui import QPixmap
+from PySide6.QtCore import Qt
+import os, subprocess, platform
 
 osName = platform.system()
+
+def openFile():
+    fileDialog = QFileDialog(window)
+    fileDialog.setWindowTitle("Select File")
+    fileDialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+    fileDialog.setViewMode(QFileDialog.ViewMode.Detail)
+
+    if fileDialog.exec():
+        file = fileDialog.selectedFiles()
+        print("Selected Wallpaper:", file[0])
+        return file[0]
 
 def detectDE(file):
     de = os.environ["XDG_CURRENT_DESKTOP"]
@@ -12,17 +24,17 @@ def detectDE(file):
             theme = subprocess.run("gsettings get org.gnome.desktop.interface color-scheme".split(), capture_output=True, text=True)
             print(theme.stdout)
             if theme.stdout.strip() == '\'prefer-dark\'':
-                cmd = f"gsettings set org.gnome.desktop.background picture-uri-dark file://{file}"
+                cmd = ["gsettings", "set", "org.gnome.desktop.background", "picture-uri-dark", f'file://{file}']
             else:
-                cmd = f"gsettings set org.gnome.desktop.background picture-uri file://{file}"
+                cmd = ["gsettings", "set", "org.gnome.desktop.background", "picture-uri", f'file://{file}']
         case "Hyprland" | "sway" | "niri":
-            cmd = f"awww img {file}"
+            cmd = ["awww", "img" f"{file}"]
 
         case "i3":
-            cmd = f"feh --bg-scale {file}"
+            cmd = ["feh", "--bg-scale", f"{file}"]
 
         case "KDE":
-            cmd = f"plasma-apply-wallpaperimage {file}"
+            cmd = ["plasma-apply-wallpaperimage", f"{file}"]
 
     print(f"DE: {de}\nCommand: {cmd}")
     return cmd
@@ -31,7 +43,7 @@ def winPaper(file):
     import ctypes
     from PIL import Image
 
-    file = os.path.abspath(file)
+    # file = os.path.abspath(file)
     img = Image.open(file)
     if img.mode != 'RGB': img = img.convert("RGB")
 
@@ -43,7 +55,8 @@ def winPaper(file):
 def changePaper():
     # file = "/home/neuro/Downloads/FrierenWallpaper.jpeg"
     # file = "c:/Users/n2194/Downloads/FrierenWallpaper.jpg"
-    file = askopenfilename(preview_img=True, autocomplete=True, filetypes=["png", 'jpeg', 'jpg', 'webp'])
+    file = openFile()
+    file = os.path.abspath(file)
     if osName == "Windows":
         print("Running on Windows")
         winPaper(file)
@@ -51,18 +64,21 @@ def changePaper():
     elif osName == "Linux":
         print("Running on Linux")
         cmd = detectDE(file)
-    elif osName == "Darwin":
-        print("Running on macOS")
-        cmd = f"osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file {file}'"
+    # elif osName == "Darwin":
+    #     print("Running on macOS")
+    #     cmd = ["osascript", "-e", "'tell application \"Finder\" to set desktop picture to POSIX file'", f"'{file}'"
     else:
-        print(f"Running on an unidentified system: {osName}")
+        print(f"System is not supported: {osName}\n\nIf you know how to add your system, feel free to contribute.")
 
-    subprocess.run(cmd.split())
+    subprocess.run(cmd)
 
-app = ctk.CTk()
-app.geometry("512x512")
+app = QApplication([])
+window = QMainWindow()
+window.setWindowTitle("Test2")
+window.setGeometry(0, 0, 1024, 512)
 
-btn = ctk.CTkButton(app, text="Select BG", command=changePaper)
-btn.pack()
+btn = QPushButton("Open File", window)
+btn.clicked.connect(changePaper)
 
-app.mainloop()
+window.show()
+app.exec()
